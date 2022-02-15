df1cf9754cc04688e11c3683c3658975788cc564
Alexandru <alexandru.akkol@gmail.com>
2022-02-15 19:47:46 +0200

    bugfixes

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from selenium.common.exceptions import TimeoutException
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-
 results = [];
 DEBUG_MODE = False
 delay = 2
@@ -12,6 +11,15 @@ delay = 2
 def get_price(driver):
     global delay
     data = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH,"/html/body/form/div[3]/div/div[1]/div[1]/div[2]/div[1]/div[2]/div[3]/div[1]/div/strong")))
+    if data.text == None:
+        return None
+    try:
+        if data.text[0].isnumeric() == False:
+            print("Price is not numeric")
+            return None
+    except IndexError:
+        return None
+
     return float(data.text)
 
 def get_cur_assets(driver):
@@ -30,7 +38,8 @@ def get_cur_assets(driver):
             #print(f"Current assets: {current_assets}.")
     except TimeoutException:
         print(f"Loading took too much time!")
-
+    if current_assets == None:
+        return None
     return int(current_assets.replace(",", ""))
 
 def get_shares_outs(driver):
@@ -42,6 +51,8 @@ def get_shares_outs(driver):
         if item.isdigit():
             shares_outstanding += item
 
+    if shares_outstanding == None:
+        return None
     return int(shares_outstanding)
 
 def get_st_liab(driver):
@@ -74,9 +85,12 @@ def get_st_liab(driver):
     except TimeoutException:
         print(f"Loading took too much time!")
 
+    if total_st_liab == None:
+        return None
+
     if total_st_liab[0].isnumeric() == False:
         print("ST is not numeric")
-        return 0
+        return None
     if rev_advance:
         if rev_advance[0].isnumeric():
             return int(rev_advance.replace(",", "")) + int(total_st_liab.replace(",", ""))
@@ -101,9 +115,14 @@ def get_lt_liab(driver):
             #print(f"Longterm debt: {total_lt_liab}.")
     except TimeoutException:
         print(f"Loading took too much time!")
+
+    if total_lt_liab == None:
+        return None
+
     if total_lt_liab[0].isnumeric() == False:
         print("LT is not numeric")
-        return 0
+        return None
+
     return int(total_lt_liab.replace(",", ""))
 
 
@@ -124,12 +143,11 @@ def get_financials(driver):
     return [price, shares, cur_assets, st_liab, lt_liab]
 
 def compute_NNR(price, shares, cur_assets, st_liab, lt_liab):
-    if st_liab == None:
-        st_liab = 0
-    if lt_liab == None:
-        lt_liab = 0
-    return round((((cur_assets - (st_liab + lt_liab)) / shares) / price) * 100 , 2)
 
+    if price == None or shares == None or cur_assets == None or st_liab == None or lt_liab == None:
+        return "N/A"
+    else:
+        return round((((cur_assets - (st_liab + lt_liab)) / shares) / price) * 100, 2)
 # get symbols list
 with open("symbols.txt", "r") as file:
     symbols = file.read().splitlines()
