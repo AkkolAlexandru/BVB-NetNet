57b46e5aa727c6230942ee4480185f844cc7e1ca
Alexandru <alexandru.akkol@gmail.com>
2022-02-17 19:22:02 +0200

    v2 mvp in prog

---
+++ financials.py
@@ -0,0 +1,184 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+
+results = []
+DEBUG_MODE = True
+delay = 2
+
+def get_status(driver):
+    try:
+        status = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH,"""/html/body/form/div[3]/div/div[1]/div[1]/div[2]/div[1]/div[1]/table/tbody/tr[6]/td[2]"""))).text
+
+    except TimeoutException:
+        print(f"Loading took too much time!")
+
+    return status
+
+def get_price(driver):
+    global delay
+    data = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH,"/html/body/form/div[3]/div/div[1]/div[1]/div[2]/div[1]/div[2]/div[3]/div[1]/div/strong")))
+
+   #Exceptions
+    if data.text == None:
+        print("price error")
+        return None
+    try:
+        if data.text[0].isnumeric() == False:
+            print("Price is not numeric")
+            return None
+    except IndexError:
+        print("price error")
+        return None
+
+    return float(data.text)
+
+def get_cur_assets(driver):
+    find = None
+    global delay
+    try:
+        find = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, """//td[contains(.,'Total Current Assets')]/following::td"""))).text
+    except TimeoutException:
+        try:
+            find = WebDriverWait(driver, delay).until(EC.presence_of_element_located(
+                (By.XPATH, """//td[contains(.,'Current assets - Total')]/following::td"""))).text
+        except TimeoutException:
+            try:
+                find = WebDriverWait(driver, delay).until(EC.presence_of_element_located(
+                    (By.XPATH, """//td[contains(.,'Cash')]/following::td"""))).text
+            except TimeoutException:
+                return None
+
+    return int(find.replace(",", ""))
+
+def get_shares_outs(driver):
+    global delay
+    shares_outstanding = str()
+    data = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, """/ html / body / form / div[3] / div / div[1] / div[3] / div / div[2] / div / div[5] / div / table / tbody / tr[1] /td[2]""")))
+    #convert txt to number and remove commas
+    for item in data.text:
+        if item.isdigit():
+            shares_outstanding += item
+
+    #Exceptions
+    if shares_outstanding == None:
+        print("shares outstanding error")
+        return None
+    return int(shares_outstanding)
+
+def get_st_liab(driver):
+    global delay
+    t_st_liab = str()
+
+    # scraping block
+    try:
+        t_st_liab = WebDriverWait(driver, delay).until(
+            EC.presence_of_element_located((By.XPATH, """//td[contains(.,'Total Current Liabilities')]/following::td"""))).text
+    except TimeoutException:
+        try:
+            debtors = WebDriverWait(driver, delay).until(
+                EC.presence_of_element_located(
+                    (By.XPATH, """//td[contains(.,'Debtors - due within one year')]/following::td"""))).text
+        except TimeoutException:
+            debtors = 0
+
+    try:
+        rev_advance = WebDriverWait(driver, delay).until(
+            EC.presence_of_element_located(
+                (By.XPATH, """//td[contains(.,'Revenues in advance')]/following::td"""))).text
+    except TimeoutException:
+        rev_advance = 0
+
+    if t_st_liab:
+        t_st_liab = t_st_liab.replace(",", "")
+    if debtors:
+        debtors = debtors.replace(",", "")
+    if rev_advance:
+        rev_advance = rev_advance.replace(",", "")
+
+    if DEBUG_MODE:
+        print(f"t_st_liab: {t_st_liab}")
+        print(f"debtors: {debtors}")
+        print(f"rev_advance: {rev_advance}")
+
+    if t_st_liab:
+        if t_st_liab[0].isnumeric() is False:
+            if debtors or rev_advance:
+                return int(rev_advance) + int(debtors)
+            else:
+                return None
+    else:
+        if debtors or rev_advance:
+            if rev_advance[0].isnumeric():
+                return int(rev_advance) + int(debtors)
+            else:
+                return int(debtors)
+        else:
+            return None
+
+
+    return int(t_st_liab)
+
+def get_lt_liab(driver):
+
+    total_lt_liab = None
+    try:
+        total_lt_liab = WebDriverWait(driver, delay).until(
+            EC.presence_of_element_located(
+                (By.XPATH, """//td[contains(.,'Total Longterm Liabilities')]/following::td"""))).text
+    except TimeoutException:
+        try:
+            total_lt_liab = WebDriverWait(driver, delay).until(
+                EC.presence_of_element_located(
+                    (By.XPATH, """//td[contains(.,'Debtors - due after more than one year')]/following::td"""))).text
+        except TimeoutException:
+            return None
+
+    if total_lt_liab:
+        total_lt_liab = total_lt_liab.replace(",", "")
+
+    if DEBUG_MODE:
+        print(f"total_lt_liab: {total_lt_liab}")
+
+
+    return int(total_lt_liab)
+
+#todo test
+def alternative_Total_Liab(driver):
+    alt_total_liab = None
+    try:
+        alt_total_liab = WebDriverWait(driver, delay).until(
+            EC.presence_of_element_located(
+                (By.XPATH, """//td[contains(.,'Total liabilities')]/following::td"""))).text
+    except TimeoutException:
+        try:
+            alt_total_liab = WebDriverWait(driver, delay).until(
+                EC.presence_of_element_located(
+                    (By.XPATH, """//td[contains(.,'Total Liabilities')]/following::td"""))).text
+        except TimeoutException:
+            return None
+
+    if alt_total_liab:
+        alt_total_liab = alt_total_liab.replace(",", "")
+
+    if DEBUG_MODE:
+        print(f"alt_total_liab: {alt_total_liab}")
+
+    return int(alt_total_liab)
+
+def get_financials(driver):
+    price = get_price(driver)
+    shares = get_shares_outs(driver)
+    try:
+        financial_tab = WebDriverWait(driver, delay).until(EC.presence_of_element_located(
+            (By.XPATH, """/html/body/form/div[3]/div/div[1]/div[2]/div/div[1]/div/div/input[5]""")))
+        financial_tab.click()
+    except TimeoutException:
+        print(f"Loading took too much time!")
+
+    cur_assets = get_cur_assets(driver)
+    st_liab = get_st_liab(driver)
+    lt_liab = get_lt_liab(driver)
+
+    return [price, shares, cur_assets, st_liab, lt_liab]
--- main.py
+++ main.py
@@ -1,190 +1,34 @@
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-results = [];
-DEBUG_MODE = False
-delay = 2
-
-def get_price(driver):
-    global delay
-    data = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH,"/html/body/form/div[3]/div/div[1]/div[1]/div[2]/div[1]/div[2]/div[3]/div[1]/div/strong")))
-
-   #Exceptions
-    if data.text == None:
-        print("price error")
-        return None
-    try:
-        if data.text[0].isnumeric() == False:
-            print("Price is not numeric")
-            return None
-    except IndexError:
-        print("price error")
-        return None
-
-    return float(data.text)
-
-def get_cur_assets(driver):
-    current_assets = None
-    global delay
-    try:
-        # if type1
-
-        if WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, """/html/body/form/div[3]/div/div[1]/div[3]/div/div[1]/div[1]/div/div/table/tbody/tr[4]/td[1]"""))).text == "Total Current Assets":
-            current_assets = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH,"""/html/body/form/div[3]/div/div[1]/div[3]/div/div[1]/div[1]/div/div/table/tbody/tr[4]/td[2]"""))).text
-            #print(f"Current assets: {current_assets}.")
-        # if type2
-
-        if WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, """/html/body/form/div[3]/div/div[1]/div[3]/div/div[1]/div/div/div/table/tbody/tr[2]/td[1]"""))).text == "Current assets - Total":
-            current_assets = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH,"""/html/body/form/div[3]/div/div[1]/div[3]/div/div[1]/div/div/div/table/tbody/tr[2]/td[2]"""))).text
-            #print(f"Current assets: {current_assets}.")
-    except TimeoutException:
-        print(f"Loading took too much time!")
-
-    #Exceptions
-    if current_assets == None:
-        print("current asset error")
-        return None
-    return int(current_assets.replace(",", ""))
-
-def get_shares_outs(driver):
-    global delay
-    shares_outstanding = str()
-    data = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, """/ html / body / form / div[3] / div / div[1] / div[3] / div / div[2] / div / div[5] / div / table / tbody / tr[1] /td[2]""")))
-#convert txt to number and remove commas
-    for item in data.text:
-        if item.isdigit():
-            shares_outstanding += item
-
-    #Exceptions
-    if shares_outstanding == None:
-        print("shares outstanding error")
-        return None
-    return int(shares_outstanding)
-
-def get_st_liab(driver):
-    total_st_liab = None
-    rev_advance = None
-    global delay
-    try:
-
-        # if type1
-        if WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH,
-                                                                              """/html/body/form/div[3]/div/div[1]/div[3]/div/div[1]/div[1]/div/div/table/tbody/tr[10]/td[1]"""))).text == "Total Current Liabilities":
-            total_st_liab = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH,
-                                                                                               """/html/body/form/div[3]/div/div[1]/div[3]/div/div[1]/div[1]/div/div/table/tbody/tr[10]/td[2]"""))).text
-            # print(f"Shortterm liabilities: {total_st_liab}.")
-
-        # if type2
-        if WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH,
-                                                                              """/html/body/form/div[3]/div/div[1]/div[3]/div/div[1]/div/div/div/table/tbody/tr[3]/td[1]"""))).text == "Debtors - due within one year":
-            total_st_liab = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH,
-                                                                                               """/html/body/form/div[3]/div/div[1]/div[3]/div/div[1]/div/div/div/table/tbody/tr[3]/td[2]"""))).text
-            #print(f"Shortterm debt: {total_st_liab}.")
-
-        # if revenue advance
-        if WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH,
-                                                                              """/html/body/form/div[3]/div/div[1]/div[3]/div/div[1]/div/div/div/table/tbody/tr[7]/td[1]"""))).text == "Revenues in advance":
-            rev_advance = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH,
-                                                                                             """/html/body/form/div[3]/div/div[1]/div[3]/div/div[1]/div/div/div/table/tbody/tr[7]/td[2]"""))).text
-            # print(f"Revenue in advance: {rev_advance}.")
-
-    except TimeoutException:
-        print(f"Loading took too much time!")
-
-    #Exceptions
-    if total_st_liab == None:
-        print("ST Liabilities error")
-        return None
-
-    if total_st_liab[0].isnumeric() == False:
-        print("ST is not numeric")
-        return None
-    if rev_advance:
-        if rev_advance[0].isnumeric():
-            return int(rev_advance.replace(",", "")) + int(total_st_liab.replace(",", ""))
-        else:
-            rev_advance = 0
-    else:
-        return int(total_st_liab.replace(",", ""))
-
-
-def get_lt_liab(driver):
-
-    total_lt_liab = None
-    try:
-        # if type1
-        if WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH,"""/html/body/form/div[3]/div/div[1]/div[3]/div/div[1]/div[1]/div/div/table/tbody/tr[15]/td[1]"""))).text == "Total Longterm Liabilities":
-            total_lt_liab = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH,"""/html/body/form/div[3]/div/div[1]/div[3]/div/div[1]/div[1]/div/div/table/tbody/tr[15]/td[2]"""))).text
-            #print(f"Longterm liabilities: {total_lt_liab}.")
-
-        # if type2
-        if WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH,"""/html/body/form/div[3]/div/div[1]/div[3]/div/div[1]/div/div/div/table/tbody/tr[6]/td[1]"""))).text == "Debtors - due after more than one year":
-            total_lt_liab = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH,"""/html/body/form/div[3]/div/div[1]/div[3]/div/div[1]/div/div/div/table/tbody/tr[6]/td[2]"""))).text
-            #print(f"Longterm debt: {total_lt_liab}.")
-    except TimeoutException:
-        print(f"Loading took too much time!")
-
-    #Exceptions
-    if total_lt_liab == None:
-        print("LT Liabilities error")
-        return None
-
-    if total_lt_liab[0].isnumeric() == False:
-        print("LT is not numeric")
-        return None
-
-    return int(total_lt_liab.replace(",", ""))
-
-
-def get_financials(driver):
-    price = get_price(driver)
-    shares = get_shares_outs(driver)
-    try:
-        financial_tab = WebDriverWait(driver, delay).until(EC.presence_of_element_located(
-            (By.XPATH, """/html/body/form/div[3]/div/div[1]/div[2]/div/div[1]/div/div/input[5]""")))
-        financial_tab.click()
-    except TimeoutException:
-        print(f"Loading took too much time!")
-
-    cur_assets = get_cur_assets(driver)
-    st_liab = get_st_liab(driver)
-    lt_liab = get_lt_liab(driver)
-
-    return [price, shares, cur_assets, st_liab, lt_liab]
+from financials import get_financials
 
 def compute_NNR(price, shares, cur_assets, st_liab, lt_liab):
-
+    # TODO remove this
+    print(st_liab, type(st_liab))
     if price == None or shares == None or cur_assets == None or st_liab == None or lt_liab == None:
         return "N/A"
     else:
         return round((((cur_assets - (st_liab + lt_liab)) / shares) / price) * 100, 2)
+
 # get symbols list
 with open("symbols.txt", "r") as file:
     symbols = file.read().splitlines()
 
-def run_scraper(symbol):
-    url = f"https://bvb.ro/FinancialInstruments/Details/FinancialInstrumentsDetails.aspx?s={symbol}"
-    driver = webdriver.Chrome()
-    driver.get(url)
-    fin = get_financials(driver)
-    driver.quit()
-    nnr = compute_NNR(fin[0],fin[1],fin[2],fin[3],fin[4])
-    if DEBUG_MODE:
-        print(f"[{symbol}] Price = {fin[0]}")
-        print(f"[{symbol}] Shares = {fin[1]}")
-        print(f"[{symbol}] Current assets = {fin[2]}")
-        print(f"[{symbol}] ST Liabilities = {fin[3]}")
-        print(f"[{symbol}] LT Liabilities = {fin[4]}")
-        print(f"[{symbol}] NNR Ratio = {nnr}")
-    return {symbol:nnr}
+#TODO
+symbol="mecp"
+############
+url = f"https://bvb.ro/FinancialInstruments/Details/FinancialInstrumentsDetails.aspx?s={symbol}"
+driver = webdriver.Chrome()
+driver.get(url)
 
-for symbol in symbols:
-    symbol="sif1"
-    results.append(run_scraper(symbol))
-    print(results)
+
+if get_status(driver) != 'Tradeable':
+    print(get_status(driver))
+    return {symbol:"delisted"}
+
+fin = get_financials(driver)
+print()
+
+
 
 
 
